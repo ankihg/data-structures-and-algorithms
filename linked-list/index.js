7b6cc11b825140d230dc2f3b485cdb3a4b0c7828
ankihg <hagelin.annika@gmail.com>
2016-05-10 00:07:14 -0700

    append

--- linked-list/index.js
+++ linked-list/index.js
@@ -1,4 +1,25 @@
-const Node = function(value, next) {
-  this.value = value;
-  this.next = next || null;
-}
+'use strict';
+const node = class {
+
+  constructor(value, next) {
+    this.value = value;
+    this.next = next || null;
+  }
+
+  append(value) {
+    if (!this.next) return this.next = new node(value);
+    return this.next.append(value);
+  }
+
+  insert(value, index) {
+
+  }
+
+};
+
+var list = new node(1);
+console.log(list);
+list.append(3);
+console.log(list);
+list.append(5);
+console.log(list);
